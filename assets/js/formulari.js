7b9ff2c90503d4e512de381fb02030de8a9c7e60
mohamedamine724 <33926500+mohamedamine724@users.noreply.github.com>
2017-11-23 10:40:49 +0100

    Initial commit

---
+++ assets/js/formulari.js
@@ -0,0 +1,9 @@
+
+		function enviar()
+		{
+			alert("Gràcies per confiar en nosaltres");
+		}
+		window.onload = function(){
+			var btn = document.getElementById("enviar");
+			btn.onclick = enviar;
+		}
